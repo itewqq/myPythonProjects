8ca7a2cbf2aae59825df876ac1f0b52d74b85bce
qushipei <30570177+qushipei@users.noreply.github.com>
2019-01-08 17:55:45 +0800

    Update educard.py

--- educard.py
+++ educard.py
@@ -10,16 +10,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pytesseract
 from PIL import Image
+import os
+#from PyQt5.QtWidgets import *
+#from PyQt5 import QtCore, QtGui,  Qt
 
-debug=1
+#from PyQt5.QtWidgets import *
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+#from PyQt5.QtCore import *
+
+import pandas as pd
+
+debug=0
 lot=170
-imgPath="testPic3.jpg"
+#imgPath="testPic3.jpg"
 
 def stechCr(img):
     dst = cv2.equalizeHist(img)
-    cv2.imshow('dst',dst)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    if debug:
+        cv2.imshow('dst',dst)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
     return dst
 
 
@@ -42,7 +53,6 @@ def makepic(Img):
     box = cv2.boxPoints(rect)
     box = np.int0(box)
     h = abs(box[0][1] - box[2][1])
-    
     w = abs(box[0][0] - box[2][0])
     Xs = [i[0] for i in box]
     Ys = [i[1] for i in box]
@@ -57,11 +67,10 @@ def makepic(Img):
     return r1,r2
 
 def prepare(gray):
-    ret, binary = cv2.threshold(gray, lot, 255, cv2.THRESH_BINARY_INV)
-    #膨胀操作
+    ret, binary = cv2.threshold(gray, lot, 255, cv2.THRESH_BINARY_INV)#反向二值化
     opening=binary
     ele = cv2.getStructuringElement(cv2.MORPH_RECT, (17, 17))
-    dilation = cv2.dilate(opening, ele, iterations=1)
+    dilation = cv2.dilate(opening, ele, iterations=1)#膨胀操作
     cv2.imwrite("BI_img.png", binary)
     cv2.imwrite("AfterDilate.png", dilation)
     if debug :
@@ -77,35 +86,28 @@ def prepare(gray):
 
 def findTextRegion(img):
     region = []
-    # 查找轮廓
+    # 查找膨胀块轮廓
     image, contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     for i in range(len(contours)):
         cnt = contours[i]
-        # 计算该轮廓的面积
         area = cv2.contourArea(cnt)
-        # 面积过大过小的都去掉掉
+        # 面积过大过小的都去掉
         if (area < 4*1500 or area >2*15000):#调参使得适合一卡通
             continue
         # 找到最小的矩形，该矩形可能有方向
         rect = cv2.minAreaRect(cnt)
-        #函数 cv2.minAreaRect() 返回一个Box2D结构 rect：（最小外接矩形的中心（x，y），（宽度，高度），旋转角度）。
-#        if debug:
-#            print("rect is: ", rect)
         # box是四个点的坐标
         box = cv2.boxPoints(rect)
         box = np.int0(box)
-        # 计算高和宽
         height = abs(box[0][1] - box[2][1])
         width = abs(box[0][0] - box[2][0])
         # 筛选矩形
         if (height > width * 1.2):
             continue
-
         if (height * 15 < width):
             continue
         #if (width > img.shape[1] / 2 and height > img.shape[0] / 20):
         region.append(box)
-        
     return region
 
 
@@ -113,19 +115,18 @@ def detect(img):
     gray = cv2.fastNlMeansDenoisingColored(img, None, 10, 3, 3, 3)
     gray=cv2.cvtColor(gray,cv2.COLOR_BGR2GRAY)
     img,gray=makepic(gray)
-    cv2.imshow('tmp1',img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    cv2.imshow('tmp2',gray)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
     if debug:
-        cv2.imwrite("gray.png", gray)
+        cv2.imshow('tmp1',img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        cv2.imshow('tmp2',gray)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    cv2.imwrite("gray.png", gray)
     #二值化和膨胀变换
     dilation = prepare(gray)
-    if debug:
-        cv2.imwrite('dilation.png',dilation)
+    cv2.imwrite('dilation.png',dilation)
     #文字区域
     region = findTextRegion(dilation)
     #画出文本框
@@ -133,6 +134,7 @@ def detect(img):
     i=0
     for box in region:
         h = abs(box[0][1] - box[2][1])
+
         w = abs(box[0][0] - box[2][0])
         Xs = [i[0] for i in box]
         Ys = [i[1] for i in box]
@@ -150,7 +152,16 @@ def detect(img):
 
 
 def ocrIdCard(imgPath):
-    img = cv2.imread(imgPath)
+    fc=len(imgPath)
+    pre="";af=""
+    for i in range(fc-1,-1,-1):
+        if imgPath[i]=="/":
+            pre=imgPath[:i]
+            af=imgPath[i+1:]
+            break
+    os.chdir(pre)
+    it=os.getcwd()
+    img = cv2.imread(af)
     infoDict={"学号":"",
               "姓名":"",
               "学院":"",
@@ -158,14 +169,16 @@ def ocrIdCard(imgPath):
             }
     print('start...\n')
     idImgs = detect(img)
+    
     tessdata_dir_config = '--tessdata-dir "C:\\Program Files (x86)\\Tesseract-OCR\\tessdata"'
     tesseract_cmd = "C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe"
     i=1
     for idImg in idImgs:
         #print(i)
-        cv2.imshow('temp'+str(i),idImg)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        if debug:
+            cv2.imshow('temp'+str(i),idImg)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
         image = Image.fromarray(idImg)
         result = pytesseract.image_to_string(image, lang='chi_sim',config=tessdata_dir_config,)
         result=result.replace(' ','')
@@ -188,10 +201,81 @@ def ocrIdCard(imgPath):
     return infoDict
 
 
-if __name__=="__main__":
-    print("kaishi")
-    ans=ocrIdCard(imgPath)
-    print(ans)
+class oneCardOcrUi(QtWidgets.QWidget):
+
+    def __init__(self):
+        super(oneCardOcrUi,self).__init__()
+        self.setupUi()
+        self.infoCard={}
+
+    def setupUi(self):
+        self.setWindowTitle("一卡通识别")
+        self.layout = QtWidgets.QGridLayout()
+        layout=self.layout
+        self.setGeometry(600, 600, 400, 400)
+
+        
+        self.nameLabel = QtWidgets.QLabel("姓名")
+        self.nameLineEdit = QtWidgets.QLineEdit("")
+        self.idLabel = QtWidgets.QLabel("学号")
+        self.idLineEdit = QtWidgets.QLineEdit("")
+        self.scLabel = QtWidgets.QLabel("学院")
+        self.scLineEdit = QtWidgets.QLineEdit("")
+        self.clLabel = QtWidgets.QLabel("班级")
+        self.clLineEdit = QtWidgets.QLineEdit("")
+        
+        
+        layout.addWidget(self.nameLabel,1,0)
+        layout.addWidget(self.nameLineEdit,1,1)
+        layout.addWidget(self.idLabel, 2, 0)
+        layout.addWidget(self.idLineEdit, 2, 1)
+        layout.addWidget(self.scLabel,3,0)
+        layout.addWidget(self.scLineEdit,3,1)
+        layout.addWidget(self.clLabel,4,0)
+        layout.addWidget(self.clLineEdit,4,1)
+        
+        layout.setColumnStretch(1, 10)
+        open_Btn = QtWidgets.QPushButton('打开图片')
+        save_Btn = QtWidgets.QPushButton('保存为csv')
+        save_Btn.clicked.connect(self.savefile)
+        open_Btn.clicked.connect(self.openfile)
+        
+        layout.addWidget(open_Btn)
+        layout.addWidget(save_Btn)
+        self.setLayout(layout)
+        self.show()
+    def savefile(self):
+        savefile_name=QtWidgets.QFileDialog.getSaveFileName(self,'选择存储方式','','csv文件(*.csv )')
+        if debug:
+            print(savefile_name)
+            print(self.infoCard)
+        if savefile_name is None:
+            return
+        dataframe = pd.DataFrame(self.infoCard,index=[0])
+        dataframe.to_csv(savefile_name[0],index=[0],sep=',',encoding="utf_8_sig")
+        
+    def openfile(self):
+        openfile_name = QtWidgets.QFileDialog.getOpenFileName(self,'选择文件','','图片文件(*.jpeg , *.png, *.jpg)')
+        if openfile_name is None:
+            return 
+        self.infoCard=ocrIdCard(openfile_name[0])
+        self.nameLineEdit.setText(self.infoCard["姓名"])
+        self.idLineEdit.setText(self.infoCard["学号"])
+        self.scLineEdit.setText(self.infoCard["学院"])
+        self.clLineEdit.setText(self.infoCard["班级"])
+        
+        if debug:
+            print(self.infoCard)
+            print(self.nameLineEdit.text())
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    ui = oneCardOcrUi()
+    #ui.setupUi()这两行坑了我好久。。。mmp写在外面就不能实时刷新文本框了
+    #ui.show()
+    sys.exit(app.exec_())
+
 
 
 
